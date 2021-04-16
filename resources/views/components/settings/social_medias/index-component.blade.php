5fc4e4fbb5426f49342529313fc1a2df683832f7
BILD\jean.garcia <jean.garcia@bild.com.br>
2021-04-16 19:52:39 -0300

    add info na view

--- resources/views/components/settings/social_medias/index-component.blade.php
+++ resources/views/components/settings/social_medias/index-component.blade.php
@@ -5,43 +5,51 @@
 ?>
 
 
-
-
 <div class="section">
     <div id="responsive-table" class="card card card-default scrollspy">
         <div class="card-content">
             <div class="row right-align">
-          <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
-  <!-- Dropdown Structure -->
-  <ul id='dropdown1' class='dropdown-content'>
-  <li><a href="#!">one</a></li>
-  <li><a href="#!">two</a></li>
-  <li class="divider"></li>
-  <li><a href="#!">three</a></li>
-  <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-  <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-  </ul>
-    </div>
+                <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+                <!-- Dropdown Structure -->
+                <ul id='dropdown1' class='dropdown-content'>
+                    <li><a href="#!">one</a></li>
+                    <li><a href="#!">two</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#!">three</a></li>
+                    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+                    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+                </ul>
+            </div>
             <div class="row">
-                <table class="responsive-table">
+                <table class="responsive-table Highlight">
                     <thead>
                     <tr>
+                        <th data-field=""></th>
                         <th data-field="name">{{ __('Usuário') }}</th>
                         <th data-field="status">{{ __('Seguidores') }}</th>
                         <th data-field="status">{{ __('Seguindo') }}</th>
                         <th data-field="status">{{ __('Publicações') }}</th>
+                        <th data-field="status">{{ __('Tags') }}</th>
                         <th data-field="status">{{ __('Ações') }}</th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="tab-content">
 
 
                     @foreach($socialMediaAccounts->all() as $socialMediaAccount)
                         <tr>
-                            <td><i class="fab fa-{{ $socialMediaAccount->social_media->slug }} fa-2x"></i><a href="https://www.instagram.com/{{ $socialMediaAccount->username }}" target="_blank"><span class="text uppercase">{{ $socialMediaAccount->username }}</span></a></td>
-                            <td>999.999.99</td>
-                            <td>999.999.99</td>
-                            <td>999.999.99</td>
+                            <td class="center-align"><img class="circle" width="20%" src="{{ $socialMediaAccount->data->user->profile_pic_url }}" alt="">
+
+                            </td>
+                            <td><a href="https://www.instagram.com/{{ $socialMediaAccount->username }}" target="_blank">
+                                    <span class="text uppercase">{{ $socialMediaAccount->username }}
+                                    </span>
+                                </a></td>
+                            <td>{{ number_format($socialMediaAccount->data->user->follower_count, thousands_separator: '.') }}</td>
+                            <td>{{ number_format($socialMediaAccount->data->user->following_count, thousands_separator: '.') }}</td>
+                            <td>{{ number_format($socialMediaAccount->data->user->media_count, thousands_separator: '.') }}</td>
+                            <td>{{ number_format($socialMediaAccount->data->user->following_tag_count, thousands_separator: '.') }}</td>
+
                             <td>
                                 <a href="{{ route('settings.social_medias.instagram.edit', [$socialMediaAccount->id]) }}">
                                     <i class="fa fa-edit fa-lg"></i>
